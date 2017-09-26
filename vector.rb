5353868c6bb6786649a0abba47094e7f7db6a7ff
j1130 <j1130@PC4-21.cc.matsue-ct.jp>
2017-09-26 11:44:04 +0900

    vector.rbを変更

--- vector.rb
+++ vector.rb
@@ -1,14 +1,20 @@
 class Vector
+  attr_accessor :x, :y
+  
   def initialize(x, y)
-    @x = x
-    @y = y
+     @x = x
+     @y = y
   end
 
   def to_s
-    "(#{@x}, #{@y})"
+     "(#{@x}, #{@y})"
   end
 
   def length
-    Math.sqrt(@x**2 + @y**2)
+     Math.sqrt(@x**2 + @y**2)
+  end
+
+  def add(v2)
+    Vector.new(@x + v2.x, @y + v2.y)
   end
 end
